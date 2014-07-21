d2cac692205a3102935068105653cb50b69225ea
= <gcpeters@gmail.com>
2014-07-20 22:23:42 -0400

    updated server config

--- server.js
+++ server.js
@@ -2,8 +2,13 @@ var express = require('express'),
 	app = express(),
 	server;
 
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
 app.use(express.static(__dirname + '/public'));
 
-server = app.listen(8000, function() {
+server = app.listen(process.env.PORT || 8000, function() {
 	console.log('Listening on port %d', server.address().port);
 });
