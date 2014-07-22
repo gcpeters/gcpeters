53f4dd5352da527bee31ef679e55a3664950795a
= <gcpeters@gmail.com>
2014-07-21 22:29:04 -0400

    Added moment lib and moved compnent logic into card, added live twitter feed

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 var express = require('express'),
+    Twit = require('twit'),
+	http = require('http'),
 	app = express(),
+	twit,
 	server;
+
+twit = new Twit({
+	consumer_key: 'YgtLZzwULKFFkIHP67oj6pKVw',
+	consumer_secret: 'lMVjabxAFl0A353IuvK6gZoarrzWPzBlo4ATfo4xh9EAwBXfIy',
+	access_token: '16514015-dA2tUX5swRxyKzy8LU3SFXDQpcqfgO8rKkAXQ8OJV',
+	access_token_secret: '5HYsGcQbn7gwpytLHitZBWkLY67Qtc9UMJTCJeTvGf29D'
+});
 
 app.use(express.favicon());
 app.use(express.logger('dev'));
@@ -8,6 +18,18 @@ app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(__dirname + '/public'));
+
+app.get('/connectors/twitter-activity', function (req, res) {
+	twit.get('statuses/user_timeline', function (err, data, response) {
+		if (err) {
+
+			console.log(err);
+			return;
+		}
+
+		res.send(data);
+	});
+});
 
 server = app.listen(process.env.PORT || 80, function() {
 	console.log('Listening on port %d', server.address().port);
