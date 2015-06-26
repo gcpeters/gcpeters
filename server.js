cf3dce721c4bbc74b6afff8cbfd8768112e7eb5d
gcpeters <gcpeters@gmail.com>
2015-06-25 22:10:49 -0400

    Updated gitignore, cleaned up some code

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 'use strict';
 
+// modules
 var _ = require('lodash'),
 	express = require('express'),
 	mongoose = require('mongoose'),
 	bodyParser = require('body-parser');
 
+// Routers
 var documentRouter = require('./app/routes/document'),
 	twitterRouter = require('./app/routes/twitter'),
 	instagramRouter = require('./app/routes/instagram');
 
-var app = express(),
-	server;
+var app = express();
 
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
@@ -20,6 +21,6 @@ app.use('/connectors/instagram-activity', instagramRouter);
 
 mongoose.connect('mongodb://localhost/');
 
-server = app.listen(80, function () {
+app.listen(80, function () {
 	console.log('Listening on port %d', server.address().port);
 });
